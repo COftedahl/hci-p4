6ba9b27cd13cdd86ae1d14a0da34bd0dc4deab7b
Cole <oftedahl.cole@gmail.com>
2026-03-29 16:21:32 -0500

    worked on sample app 1

--- src/App1/src/App.tsx
+++ src/App1/src/App.tsx
@@ -1,8 +1,6 @@
-import Calendar from "./Calendar";
-import { dateFormatter } from '../../Common/formatter';
-import allAttendance from '../data/attendance';
 import './all.css';
-import IAttendanceDay from "Types/IAttendanceDay";
+import MarkAttendancePage from './MarkAttendancePage';
+import ViewAttendancePage from './ViewAttendancePage';
 
 interface AppProps {
 
@@ -10,43 +8,29 @@ interface AppProps {
 
 const App: React.FC<AppProps> = (props: AppProps) => {
 
-  const getAttendanceFromDate = (date: string) => {
-    return allAttendance.find((dayAttendance: IAttendanceDay) => dayAttendance.date === date)
-  }
-
-  const {date, setDate} = (props as any).useState('date', dateFormatter.format(new Date(Date.UTC(2026,3,2))));
-  const {attendance, setAttendance}: {attendance: () => IAttendanceDay, setAttendance: (data: any) => void} = (props as any).useState('attendance', getAttendanceFromDate(date()));
-
-  const setDateEffect = (data: any): void => {
-    setDate(data);
-    const result = getAttendanceFromDate(data) 
-    setAttendance(result);
-  }
+  const {tab, setTab} = (props as any).useState("tab", 0)
 
   return (
-    <div className="App flexColumn gap1 spaceBetweenJustify centerAlign padding05 borderBox">
-      <Calendar selectedDate={date()} setSelectedDate={setDateEffect}/>
-      <div className="App_AttendanceDiv flexCol gap1 padding1 borderBox borderThin borderRadius">
-        <p>
-          Attendance for {date()}
-        </p>
-        <hr className="App_Attendance_Divider divider"/>
-        <div className="App_Attendance_DataDiv gap1 flexRow spaceBetweenJustify centerAlign">
-          <p>
-            Total Attendance: 
-          </p>
-          <p>
-            {attendance().totalAttendance}
-          </p>
-        </div>
-        <div className="App_Attendance_DataDiv gap1 flexRow spaceBetweenJustify centerAlign">
-          <p>
-            Students: 
-          </p>
-          <p>
-            {attendance().studentsPresent}
-          </p>
-        </div>
+    <div className="AppDiv flexColumn fullWidth fullHeight centerAlign topJustify">
+      <div className="TabGroup flexRow gap1">
+        <button className="Tab_ViewAttendanceButton button" onClick={() => setTab(0)}>
+          View Attendance
+        </button>
+        <button className="Tab_MarkAttendanceButton button" onClick={() => setTab(1)}>
+          Mark Attendance
+        </button>
+      </div>
+      <div className="App_ContentDiv">
+        {tab() === 0 ? 
+          <ViewAttendancePage/>
+        : 
+          ""
+        }
+        {tab() === 1 ?
+          <MarkAttendancePage/>
+        :
+          ""
+        }
       </div>
     </div>
   );
---
+++ src/App1/src/MarkAttendancePage.tsx
@@ -0,0 +1,63 @@
+import Calendar from "./Calendar";
+import { dateFormatter } from '../../Common/formatter';
+import allStudents from '../data/AllStudents';
+import './MarkAttendancePage.css';
+
+interface MarkAttendancePageProps {
+
+}
+
+const MarkAttendancePage: React.FC<MarkAttendancePageProps> = (props: MarkAttendancePageProps) => {
+
+  const {date, setDate} = (props as any).useState('date', dateFormatter.format(new Date(Date.UTC(2026,3,2))));
+  const {studentList, setStudentList}: {studentList: () => string[], setStudentList: (data: any) => void} = (props as any).useState('studentList', []);
+
+  const setDateEffect = (data: any): void => {
+    setDate(data);
+    setStudentList([]);
+  }
+  
+  const handleChange = (student: string) => {
+    const index: number = studentList().indexOf(student);
+
+    const newStudentList: string[] = studentList();
+    if (index >= 0) {
+      newStudentList.splice(index, 1);
+      setStudentList(newStudentList)
+    } 
+    else {
+      newStudentList.push(student);
+      setStudentList(newStudentList)
+    }
+  }
+
+  return (
+    <div className="App flexColumn gap1 spaceBetweenJustify centerAlign padding05 borderBox">
+      <Calendar selectedDate={date()} setSelectedDate={setDateEffect}/>
+      <div className="App_AttendanceDiv flexCol gap1 padding1 borderBox borderThin borderRadius">
+        <p>
+          Attendance for {date()}
+        </p>
+        <p>
+          Total Attendance: {studentList().length}
+        </p>
+        <hr className="App_Attendance_Divider divider"/>
+        <div className="App_Attendance_MarkersDiv overflowAuto">
+
+        </div>
+        {allStudents.map((student: string, index: number) => {
+          return (
+            <div key={index} className="App_MarkAttendanceDiv flexRow fullWidth spaceBetweenJustify centerAlign gap1">
+              <p>
+                {student}
+              </p>
+              <button className={"App_MarkAttendance_Input padding1 borderBox borderRadius margin05" + (studentList().includes(student) ? " App_MarkAttendance_Input-filled" : "")} onClick={() => handleChange(student)} />
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default MarkAttendancePage;
