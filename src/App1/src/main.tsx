50be43901b56cc7938ce22d9009027881787aa70
Cole <oftedahl.cole@gmail.com>
2026-04-07 12:31:33 -0500

    added functionality to save logs to db

--- src/App1/src/main.tsx
+++ src/App1/src/main.tsx
@@ -1,5 +1,13 @@
 import App from './App';
 import '../../Utility/universalstyles_3.css';
+import { logLogs, updateConsoleLog } from './logSaver';
+
+updateConsoleLog();
+// the following are to test the new logging
+// console.log("A")
+// console.log("47")
+// console.log({abc: "def"})
+logLogs();
 
 const root: HTMLElement | null = document.getElementById('root');
 if (root !== null)
